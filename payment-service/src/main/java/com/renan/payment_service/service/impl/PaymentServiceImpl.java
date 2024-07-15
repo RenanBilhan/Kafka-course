175dcfd3b87144ec0aeffa5ef411d61e15b48272
RenanBilhan <renan.bilhan@hotmail.com>
2024-07-15 17:41:50 -0300

    Json Producer configuration and service call

--- payment-service/src/main/java/com/renan/payment_service/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/renan/payment_service/service/impl/PaymentServiceImpl.java
@@ -2,14 +2,28 @@ package com.renan.payment_service.service.impl;
 
 import com.renan.payment_service.model.Payment;
 import com.renan.payment_service.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
     @Override
+    @SneakyThrows
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT SERVICE IMPL: recebendo pagamento {}.", payment);
+        log.info("Recebi o pagamento {}.", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento");
+        kafkaTemplate.send("payment-topic", payment);
+
     }
 }
